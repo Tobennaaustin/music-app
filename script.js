fc80bac3fc4a0a97349d6a786b2b0c65e2238c60
tobenna <tobennaaustin@gmail.com>
2024-02-21 12:13:44 -0100

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+//displaying time on the screen
+function getTimeOfDay() {
+  const now = new Date();
+  const hours = now.getHours();
+
+  if (hours >= 5 && hours < 12) {
+    return "Good morning";
+  } else if (hours >= 12 && hours < 18) {
+    return "Good afternoon";
+  } else {
+    return "Good evening";
+  }
+}
+
+// Display the greeting
+const greetingElement = document.getElementById('greeting');
+const greeting = getTimeOfDay();
+greetingElement.textContent = `${greeting} #######`;
+
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: '.swiper-button-next',
+  //   prevEl: '.swiper-button-prev',
+  // },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
